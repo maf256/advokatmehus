316ed128ef1e8730a7134dd4068f6c27940b7822
maf256 <maf256@gmail.com>
2023-05-02 17:26:34 +0200

    Changed Hamburger styled from styled-componnet to styled module

--- components/navbar/Hamburger.js
+++ components/navbar/Hamburger.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import { useRef, useEffect, useState } from "react"
 import RightNav from "./RightNav"
+import styles from './Hamburger.module.css'
 
 export default function Hamburger({selectedLanguage, setSelectedLanguage }) {
     const navbarRef = useRef(null)
@@ -18,51 +19,58 @@ export default function Hamburger({selectedLanguage, setSelectedLanguage }) {
             }    
         }
     }, [hamIsOn])
-    
+    // {offset > 200 ? `${styles.Header} ${styles.Headerscroll}` : styles.Header}
+    // Hamture2
     return (
         <div ref={navbarRef}>
-            <StyledHamburger open={hamIsOn}  onClick={()=> setHamIsOn(!hamIsOn)}>
-                <div></div>
-                <div></div>
-                <div></div>
-            </StyledHamburger>
+            <div className={styles.StyledHamburger} onClick={()=> setHamIsOn(!hamIsOn)}>
+                <div className={hamIsOn ? `${styles.Hamburgerclose} ${styles.Hamtrue1}` 
+                    : styles.Hamburgerclose}>
+                </div>
+                <div className={hamIsOn ? `${styles.Hamburgerclose} ${styles.Hamtrue2}` 
+                    :`${styles.Hamburgerclose} ${styles.HamburgercloseLine2}`}>
+                </div>
+                <div className={hamIsOn ? `${styles.Hamburgerclose} ${styles.Hamture3}` 
+                    : styles.Hamburgerclose}>
+                </div>
+            </div>
             <RightNav selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} hamIsOn={hamIsOn} setHamIsOn={setHamIsOn}/> 
         </div>
     )
 }
 
-const StyledHamburger = styled.div`
-    width: 2rem;
-    height: 2rem;
-    display: none;
-    position: relative;
-    z-index: 10000;
-    cursor: pointer;
+// const StyledHamburger = styled.div`
+//     width: 2rem;
+//     height: 2rem;
+//     display: none;
+//     position: relative;
+//     z-index: 10000;
+//     cursor: pointer;
 
-    @media (max-width:900px){
-        display: flex;
-        justify-content: space-around;
-        flex-flow: column  nowrap;
-    }
-    div {
-        width: 2rem;
-        height: 0.25rem;
-        background-color: black;
-        border-radius: 10px;
-        transform-origin: 1px;
-        transition: all 0.3s linear;
-        &:nth-child(1) {
-        transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
-        }
+//     @media (max-width:900px){
+//         display: flex;
+//         justify-content: space-around;
+//         flex-flow: column  nowrap;
+//     }
+//     div {
+//         width: 2rem;
+//         height: 0.25rem;
+//         background-color: black;
+//         border-radius: 10px;
+//         transform-origin: 1px;
+//         transition: all 0.3s linear;
+//         &:nth-child(1) {
+//         transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+//         }
 
-        &:nth-child(2) {
-        transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
-        opacity: ${({ open }) => open ? 0 : 1};
-        }
+//         &:nth-child(2) {
+//         transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
+//         opacity: ${({ open }) => open ? 0 : 1};
+//         }
 
-        &:nth-child(3) {
-        transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
-        }        
-    }
+//         &:nth-child(3) {
+//         transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+//         }        
+//     }
 
-`
+// `
--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,7 +1,8 @@
 import {useState, useEffect} from 'react'
-import styled from 'styled-components'
 import Hamburger from './Hamburger.js'
 import Link from 'next/link';
+import styles from './Navbar.module.css'
+
 
 export default function Navbar ({selectedLanguage, setSelectedLanguage }) {
   const [offset, setOffset] = useState(0);
@@ -15,39 +16,10 @@ export default function Navbar ({selectedLanguage, setSelectedLanguage }) {
     }
 }, []);
 
-  
   return (
-    <Nav YOffset={offset} >
-        <Link href="/"><h1>Advokat Erling Mehus</h1> </Link>
+    <header className={offset > 200 ? `${styles.Header} ${styles.Headerscroll}` : styles.Header}  >
+        <Link href="/"><h1 className={styles.H1}>Advokat Erling Mehus</h1> </Link>
         <Hamburger selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />        
-    </Nav>
+    </header>
   )
 }
-
-const Nav = styled.header `
-  width: 100%;
-  display: flex; 
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 5% 0 5%;
-  background-color: #cde5dd;
-  position: relative;
-  /* ${({ YOffset }) => YOffset > 200 ?
-        `   height: 4rem; 
-            box-shadow: 0px 2px 5px rgb(0 0 0 / 10%);
-        `
-        :
-        `   height: 5rem; 
-            box-shadow: 0;
-        `
-    }
-   */
-  transition: all .5s ease-in-out;
-  position: fixed;
-  z-index: 1001;
-  h1{
-    display: inline;
-    color: black;
-  }
-`
